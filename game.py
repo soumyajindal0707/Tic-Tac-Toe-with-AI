eb061af8ecde202a17ddf1ae8d9c59c6354e3b72
soumyajindal0707 <98908906+soumyajindal0707@users.noreply.github.com>
2022-07-05 21:49:03 +0530

    Update game.py

--- game.py
+++ game.py
@@ -18,7 +18,6 @@ class TicTacToe():
 
     @staticmethod
     def print_board_nums():
-        # 0 | 1 | 2
         number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
         for row in number_board:
             print('| ' + ' | '.join(row) + ' |')
